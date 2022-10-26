0610915a84c98b5ab24827f0cf97bd13b9f02a36
fundDeveloper22 <openfund365@gmail.com>
2022-10-26 11:00:55 +0900

    init

---
+++ contracts/interfaces/IXXXStaking2.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// Inspired by https://github.com/smartcontractkit/defi-minimal
+pragma solidity =0.8.4;
+
+interface IXXXStaking2 {
+
+    function rewardPerToken() external view returns (uint256);
+    function earned(address account) external view returns (uint256);
+    function stake(uint256 amount) external;
+    function withdraw(uint256 amount) external;
+    function claimReward() external;
+}
