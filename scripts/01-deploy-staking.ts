483a8f9719245c7ea44021f44f22e905fd7c06a9
fundDeveloper22 <openfund365@gmail.com>
2023-01-02 07:21:26 +0900

    update reward rate
    100 -> 500

--- scripts/01-deploy-staking.ts
+++ scripts/01-deploy-staking.ts
@@ -5,7 +5,7 @@ async function main() {
   const [test_account_1] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", test_account_1.address);
   console.log("Account balance:", (await test_account_1.getBalance()).toString());
-  const XXXTokenAddress = '0xBFF79e985CFBc53E4FeAFBb2DC1d99d46dd849f9';
+  const XXXTokenAddress = '0x77e27327586E4741c8b0E33B1669da19e4Bc3818';
 
   const XXXStaking2 = await ethers.getContractFactory("XXXStaking2");
   const staking2 = await XXXStaking2.deploy(XXXTokenAddress, XXXTokenAddress);
