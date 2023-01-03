9792949822e6b1ddc789dbe2ad2ebe17c78c6f79
fundDeveloper22 <openfund365@gmail.com>
2023-01-03 15:36:50 +0900

    update reward rate

--- contracts/XXXStaking2.sol
+++ contracts/XXXStaking2.sol
@@ -12,7 +12,8 @@ error RewardOverLimit();
 
 contract XXXStaking2 is ReentrancyGuard {
 
-    uint256 public constant maxEarnedValue = 10000000 * 1e18;
+    uint256 public constant maxReward = 10000001 * 1e18;
+    uint256 public constant stopRewardAt = 10000000 * 1e18;
     uint256 public totalClaimedReward = 0;
 
     IERC20 public s_rewardsToken;
@@ -22,14 +23,14 @@ contract XXXStaking2 is ReentrancyGuard {
     // Which will be multiplied by the tokens the user staked divided by the total
     // This ensures a steady reward rate of the platform
     // So the more users stake, the less for everyone who is staking.
-    uint256 public constant REWARD_RATE = 20000000;
+    uint256 public constant REWARD_RATE = 200;
     uint256 public s_lastUpdateTime;
     uint256 public s_rewardPerTokenStored;
 
     mapping(address => uint256) public s_userRewardPerTokenPaid;
     mapping(address => uint256) public s_rewards;
 
-    uint256 public s_totalSupply;
+    uint256 public s_totalStakedSupply;
     mapping(address => uint256) public s_balances;
 
     event Staked(address indexed user, uint256 indexed amount);
@@ -41,88 +42,6 @@ contract XXXStaking2 is ReentrancyGuard {
         s_rewardsToken = IERC20(rewardsToken);
     }
 
-    /**
-     * @notice How much reward a token gets based on how long it's been in and during which "snapshots"
-     */
-    function rewardPerToken() public view returns (uint256) {
-        if (s_totalSupply == 0) {
-            return s_rewardPerTokenStored;
-        }
-        return
-            s_rewardPerTokenStored +
-            (((block.timestamp - s_lastUpdateTime) * REWARD_RATE * 1e18) / s_totalSupply);
-    }
-
-    /**
-     * @notice How much reward a user has earned
-     */
-    function earned(address account) public view returns (uint256) {
-        if (totalClaimedReward >= maxEarnedValue) {
-            return 0;
-        }
-        return
-            ((s_balances[account] * (rewardPerToken() - s_userRewardPerTokenPaid[account])) 
-                / 1e4) + s_rewards[account];
-    }
-
-    /**
-     * @notice Deposit tokens into this contract
-     * @param amount | How much to stake
-     */
-    function stake(uint256 amount)
-        external
-        updateReward(msg.sender)
-        nonReentrant
-    {
-        require(amount > 0, 'ZERO');
-        s_totalSupply += amount;
-        s_balances[msg.sender] += amount;
-        emit Staked(msg.sender, amount);
-        bool success = s_stakingToken.transferFrom(msg.sender, address(this), amount);
-        if (!success) {
-            revert TransferFailed();
-        }
-    }
-
-    /**
-     * @notice Withdraw tokens from this contract
-     * @param amount | How much to withdraw
-     */
-    function withdraw(uint256 amount) 
-        external 
-        updateReward(msg.sender) 
-        nonReentrant 
-    {
-        s_totalSupply -= amount;
-        s_balances[msg.sender] -= amount;
-        emit WithdrewStake(msg.sender, amount);
-        bool success = s_stakingToken.transfer(msg.sender, amount);
-        if (!success) {
-            revert TransferFailed();
-        }
-    }
-
-    /**
-     * @notice User claims their tokens
-     */
-    function claimReward(uint256 amount) 
-        external
-        updateReward(msg.sender)
-        nonReentrant
-    {
-        require(totalClaimedReward <= maxEarnedValue, 'LIMIT');
-        s_rewards[msg.sender] -= amount;
-        totalClaimedReward += amount;
-        emit RewardsClaimed(msg.sender, amount);
-        bool success = s_rewardsToken.transfer(msg.sender, amount);
-        if (!success) {
-            revert TransferFailed();
-        }
-    }
-
-    /********************/
-    /* Modifiers Functions */
-    /********************/
     modifier updateReward(address account) {
         s_rewardPerTokenStored = rewardPerToken();
         s_lastUpdateTime = block.timestamp;
@@ -131,11 +50,68 @@ contract XXXStaking2 is ReentrancyGuard {
         _;
     }
 
-    /********************/
-    /* Getter Functions */
-    /********************/
-    // Ideally, we'd have getter functions for all our s_ variables we want exposed, and set them all to private.
-    // But, for the purpose of this demo, we've left them public for simplicity.
+    function rewardPerToken() public view returns (uint256) {
+        if (s_totalStakedSupply == 0) {
+            return s_rewardPerTokenStored;
+        }
+        return
+            s_rewardPerTokenStored +
+            (((block.timestamp - s_lastUpdateTime) * REWARD_RATE * 1e18) / s_totalStakedSupply);
+    }
+
+    function earned(address account) public view returns (uint256) {
+        if (totalClaimedReward >= stopRewardAt) {
+            return 0;
+        }
+        return
+            ((s_balances[account] * (rewardPerToken() - s_userRewardPerTokenPaid[account])) 
+                / 1e4) + s_rewards[account];
+    }
+
+    function stake(uint256 amount)
+        external
+        updateReward(msg.sender)
+        nonReentrant
+    {
+        require(amount > 0, 'ZERO');
+        s_totalStakedSupply += amount;
+        s_balances[msg.sender] += amount;
+        emit Staked(msg.sender, amount);
+        bool success = s_stakingToken.transferFrom(msg.sender, address(this), amount);
+        if (!success) {
+            revert TransferFailed();
+        }
+    }
+
+    function withdraw(uint256 amount) 
+        external 
+        updateReward(msg.sender) 
+        nonReentrant 
+    {
+        require(amount <= s_totalStakedSupply, 'LIMIT');
+        s_totalStakedSupply -= amount;
+        s_balances[msg.sender] -= amount;
+        emit WithdrewStake(msg.sender, amount);
+        bool success = s_stakingToken.transfer(msg.sender, amount);
+        if (!success) {
+            revert TransferFailed();
+        }
+    }
+
+    function claimReward(uint256 amount) 
+        external
+        updateReward(msg.sender)
+        nonReentrant
+    {
+        require(amount <= maxReward - totalClaimedReward, 'LIMIT');
+        s_rewards[msg.sender] -= amount;
+        totalClaimedReward += amount;
+        emit RewardsClaimed(msg.sender, amount);
+        bool success = s_rewardsToken.transfer(msg.sender, amount);
+        if (!success) {
+            revert TransferFailed();
+        }
+    }
 
     function getStaked(address account) public view returns (uint256) {
         return s_balances[account];
--- scripts/01-deploy-staking.ts
+++ scripts/01-deploy-staking.ts
@@ -5,7 +5,7 @@ async function main() {
   const [test_account_1] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", test_account_1.address);
   console.log("Account balance:", (await test_account_1.getBalance()).toString());
-  const XXXTokenAddress = '0xeFC61934e133C94b898Aee6057c85E7Ecf84E6C7';
+  const XXXTokenAddress = '0xb1A79fFFBEf247893E9Ae3A094B75DF262e980A6';
 
   const XXXStaking2 = await ethers.getContractFactory("XXXStaking2");
   const staking2 = await XXXStaking2.deploy(XXXTokenAddress, XXXTokenAddress);
