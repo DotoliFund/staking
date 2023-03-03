2d0b14df5d447f7337d9f6287fa098f28442911c
fundDeveloper22 <openfund365@gmail.com>
2023-03-03 16:53:59 +0900

    Update 01-deploy-staking.ts

--- scripts/01-deploy-staking.ts
+++ scripts/01-deploy-staking.ts
@@ -5,12 +5,13 @@ async function main() {
   const [test_account_1] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", test_account_1.address);
   console.log("Account balance:", (await test_account_1.getBalance()).toString());
-  const DotoliTokenAddress = '0x52904e0B1454B8b1afC29B9A10AD2161ac454257';
+  const DotoliTokenAddress = '0x4E318f23D8F6E18aae7F237DDC57C32F3fEe8d8a';
 
   const DotoliStaking = await ethers.getContractFactory("DotoliStaking");
   const staking = await DotoliStaking.deploy(DotoliTokenAddress, DotoliTokenAddress);
   await staking.deployed();
   console.log("DotoliStaking address : ", staking.address);
+  console.log("Account balance:", (await test_account_1.getBalance()).toString());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
