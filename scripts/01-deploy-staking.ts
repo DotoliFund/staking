dde5df2058fe70b41dd031b3ef862789c94e9b69
fundDeveloper22 <openfund365@gmail.com>
2023-04-05 07:12:15 +0900

    rename value

--- scripts/01-deploy-staking.ts
+++ scripts/01-deploy-staking.ts
@@ -2,16 +2,16 @@ import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { ethers } from "hardhat"
 
 async function main() {
-  const [test_account_1] = await ethers.getSigners();
-  console.log("Deploying contracts with the account:", test_account_1.address);
-  console.log("Account balance:", (await test_account_1.getBalance()).toString());
-  const DotoliTokenAddress = '0x3CE9C63607A24785b83b3d6B3245846d402fB49b';
+  const [account] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", account.address);
+  console.log("Account balance:", (await account.getBalance()).toString());
+  const DotoliTokenAddress = '0xFd78b26D1E5fcAC01ba43479a44afB69a8073716';
 
   const DotoliStaking = await ethers.getContractFactory("DotoliStaking");
   const staking = await DotoliStaking.deploy(DotoliTokenAddress, DotoliTokenAddress);
   await staking.deployed();
   console.log("DotoliStaking address : ", staking.address);
-  console.log("Account balance:", (await test_account_1.getBalance()).toString());
+  console.log("Account balance:", (await account.getBalance()).toString());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
